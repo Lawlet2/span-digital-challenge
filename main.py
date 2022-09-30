0910f7749c22fedd5e75856e5f7a4ca901128903
Lawlet2 <ealvaradoc1300@alumno.ipn.mx>
2022-09-30 02:13:11 -0500

    feat(main): add argument parser and gamesmanager obj based on input type

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 """
 Span Digital Code Challenge
 """
+import argparse
 
+from gamesmanager import GamesManager
+
+from constants import FILENAME_INPUT_TYPE
 
 def main():
-    with open("games-results.txt") as file:
-        print(file.read())
+    """
+    Creates a GamesManager obj based on the --filename argument
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--filename", 
+        type=str,
+        default=None,
+        required=False,
+        help="Enter the file that contains the games results",
+        )
+    args = parser.parse_args()
 
+    if args.filename:
+        GamesManager(input_type=FILENAME_INPUT_TYPE, filename=args.filename)
+    else:
+        GamesManager()
 
 if __name__ == "__main__":
     main()
